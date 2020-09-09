256799ef38287005c7d9c740cef052738dd9239f
Michael Klepac <mklepac90@gmail.com>
2020-09-09 17:10:49 +0000

    wrote functionality to dynamically add tweet html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+
+    let $tweet;
+
+    for (const tweet of tweets) {
+      $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+
+  }
+
+  const createTweetElement = function(tweet) {
+
+    const date = new Date(tweet.created_at);
+
+    let $tweet = `
+    <article class="tweet">
+      <header>
+        <img src=${tweet.user.avatars}>
+        <span class="name">${tweet.user.name}</span>
+        <span class="handle">${tweet.user.handle}</span>
+      </header>
+      <p>
+        ${tweet.content.text}
+      </p>
+      <footer>
+        <span>${date}</span>
+        <span><i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
+      </footer>
+    <article>`
+            
+    return $tweet
+
+  };
+
+  renderTweets(data);
+
+});
