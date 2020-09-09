e101ab15a9ac689f8d1b6759c4809c1ef4d65e08
Michael Klepac <mklepac90@gmail.com>
2020-09-09 17:09:46 +0000

    fixed minor typo

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     const value = $( this ).val();
     const parent = $( this ).parent();
     const counter = parent.find('.counter');
-    counter.val(140 - value.length);
     let counterVal = 140 - value.length;
+    counter.val(counterVal);
     let color;
     color = counterVal < 0 ? 'red' : '#545149';
     $('output').css('color', color);
