aa9a9d48dceb6c695230fc55d94d780d3f500066
Michael Klepac <mklepac90@gmail.com>
2020-09-09 00:12:25 +0000

    char counter changes color; tweak color used for other css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  console.log('DOM has loaded...');
   $('#tweet-text').on('keyup', function() {
     const value = $( this ).val();
     const parent = $( this ).parent();
     const counter = parent.find('.counter');
     counter.val(140 - value.length);
-    let counterVal = counter.val();
-    console.log(counterVal);
-    if (counterVal < 0) {
-    }
+    let counterVal = 140 - value.length;
+    let color;
+    color = counterVal < 0 ? 'red' : '#545149';
+    $('output').css('color', color);
   })
 });
