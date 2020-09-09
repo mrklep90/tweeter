17b47376f6c7a285e4dde332e4ad0b194a3962fd
Michael Klepac <mklepac90@gmail.com>
2020-09-09 18:52:26 +0000

    AJAX get request to retrive JSON tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,67 +6,54 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
+  const createTweetElement = function(tweet) {
+    
+    const date = new Date(tweet.created_at);
+    
+    let $tweet = `
+    <article class="tweet">
+    <header>
+    <img src=${tweet.user.avatars}>
+    <span class="name">${tweet.user.name}</span>
+    <span class="handle">${tweet.user.handle}</span>
+    </header>
+    <p>
+    ${tweet.content.text}
+    </p>
+    <footer>
+    <span>${date}</span>
+    <span><i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
+    </footer>
+    <article>`
+    
+    return $tweet
+    
+  };
+  
   const renderTweets = function(tweets) {
-
+    
     let $tweet;
-
+    
     for (const tweet of tweets) {
       $tweet = createTweetElement(tweet);
       $('#tweets-container').append($tweet);
     }
+    
+  };
+  
+  const loadTweets = function() {
 
-  }
+    $.ajax({
+      url: '/tweets/',
+      method: 'GET',
+      dataType: 'JSON' 
+    }).then(function(response) {
+      renderTweets(response);
+    })
 
-  const createTweetElement = function(tweet) {
+  };
 
-    const date = new Date(tweet.created_at);
-
-    let $tweet = `
-    <article class="tweet">
-      <header>
-        <img src=${tweet.user.avatars}>
-        <span class="name">${tweet.user.name}</span>
-        <span class="handle">${tweet.user.handle}</span>
-      </header>
-      <p>
-        ${tweet.content.text}
-      </p>
-      <footer>
-        <span>${date}</span>
-        <span><i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
-      </footer>
-    <article>`
-            
-    return $tweet
-
-  }
-
-  renderTweets(data);
+  loadTweets();
 
   $('form').on('submit', (evt) => {
     evt.preventDefault();
@@ -79,7 +66,5 @@ $(document).ready(function() {
       console.log(response);
     })
   })
-
-  const 
   
 });
