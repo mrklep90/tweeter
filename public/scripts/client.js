e5780aba09fe21f7fd9d52292f1b03f6d4a4a7f3
Michael Klepac <mklepac90@gmail.com>
2020-09-10 23:03:02 +0000

    linted and added comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,9 +5,11 @@
  */
 
 $(document).ready(function() {
-
-  const createTweetElement = function(tweet) {
-
+  // ---------------------------------------------------------------------
+  // For mitigating XSS, is used during construction of tweets
+  // ---------------------------------------------------------------------
+  const textEncoder = function(string) {
+    let encodedText = '';
     const characterMap = {
       '&': '&amp;',
       '<': '&lt;',
@@ -18,19 +20,24 @@ $(document).ready(function() {
       '`': '&#x60;',
       '=': '&#x3D;'
     };
-    
-    function escape (string) {
-      return String(string).replace(/[&<>"'`=\/]/g, function (s) {
-        return characterMap[s];
-      });
+    for (let s of string) {
+      if (characterMap[s]) {
+        s = characterMap[s];
+        encodedText += s;
+      } else {
+        encodedText += s;
+      }
     }
+    return encodedText;
+  };
+  // ---------------------------------------------------------------------
+  // Builds tweet article using template
+  // ---------------------------------------------------------------------
+  const createTweetElement = function(tweet) {
+    const tweetText = textEncoder(tweet.content.text);
+    const days = Math.floor((new Date().getTime() - new Date(tweet.created_at).getTime()) / (1000 * 60 * 60 * 24));
     
-    const date = new Date(tweet.created_at);
-    const today = new Date();
-    const days = Math.floor((today.getTime() - date.getTime()) / (1000*60*60*24));
-    const tweetText = escape(tweet.content.text);
-    
-    let $tweet = `
+    const $tweet = `
     <article class="tweet">
     <header>
     <img src=${tweet.user.avatars}>
@@ -44,74 +51,81 @@ $(document).ready(function() {
     <span>${days} days ago</span>
     <span><i class="fab fa-font-awesome-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
     </footer>
-    <article>`
+    <article>`;
     
-    return $tweet
-    
+    return $tweet;
   };
-  
+  // ---------------------------------------------------------------------
+  // Attaches tweet article to tweet container
+  // ---------------------------------------------------------------------
   const renderTweets = function(tweets) {
-    
     let $tweet;
-
+    // Handles array of tweets for initial page load
     if (tweets.length > 0) {
       for (const tweet of tweets) {
         $tweet = createTweetElement(tweet);
         $('#tweets-container').append($tweet);
       }
+    // Adds single tweet to top of container
     } else {
       $tweet = createTweetElement(tweets);
       $('#tweets-container').prepend($tweet);
     }
-    
   };
-  
+  // ---------------------------------------------------------------------
+  // Retrieves JSON tweets to be built
+  // ---------------------------------------------------------------------
   const loadTweets = function(recentTweet) {
-
     $.ajax({
       url: '/tweets/',
       method: 'GET',
-      dataType: 'JSON' 
+      dataType: 'JSON'
     }).then(function(response) {
-      let sortedResponse = response.sort(function (a, b) {
+      // Sorts JSON array by creation date (newest first)
+      const sortedResponse = response.sort(function(a, b) {
         return b.created_at - a.created_at;
-      })
+      });
+      // Default behavior to pass entire array
       if (!recentTweet) {
         renderTweets(sortedResponse);
+      // Only passes the first item from JSON array if function parameter specified
       } else if (recentTweet) {
         renderTweets(sortedResponse.shift());
       }
-    })
-
+    });
   };
-
+  // ---------------------------------------------------------------------
+  // Called on page load in order to display entire tweet array
+  // ---------------------------------------------------------------------
   loadTweets();
-
+  // ---------------------------------------------------------------------
+  // New tweet submissions
+  // ---------------------------------------------------------------------
   $('form').on('submit', (evt) => {
     evt.preventDefault();
+    $('.alert').empty();
+    // Error checking
     if ($('#tweet-text').val().length > 140) {
-      $('.alert').empty();
       $('.alert').append('Tweet exceeds the 140 character limitation!');
       $('.alert').slideDown();
       $('.alert').css('display', 'flex');
     } else if ($('#tweet-text').val().length === 0) {
-      $('.alert').empty();
       $('.alert').append("You can't Tweet without a Tweet!");
       $('.alert').slideDown();
       $('.alert').css('display', 'flex');
     } else {
       $('.alert').slideUp();
-      $('.alert').empty();
-      evt.preventDefault();
+      // Adds tweet to JSON array
       $.ajax({
         url: '/tweets/',
         method: 'POST',
         data: $('form').serialize()
-      }).then(function(response) {
+      }).then(function() {
         $('form').trigger('reset');
+        // Loads the new tweet on page
         loadTweets(true);
-      })
+      });
     }
-  })
+  });
   
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,15 @@
+// ---------------------------------------------------------------------
+//  Updates the text character counter
+// ---------------------------------------------------------------------
 $(document).ready(function() {
   $('#tweet-text').on('keyup', function() {
-    const value = $( this ).val();
-    const parent = $( this ).parent();
+    const value = $(this).val();
+    const parent = $(this).parent();
     const counter = parent.find('.counter');
     let counterVal = 140 - value.length;
     counter.val(counterVal);
     let color;
     color = counterVal < 0 ? 'red' : '#545149';
     $('output').css('color', color);
-  })
+  });
 });
